d1a05aaf69112f572021ebf44bc1a4dc3e0e9419
SemenCHernykh <sam_black_00@mail.ru>
2021-09-19 13:06:21 +0700

    test
    project

--- image/views.py
+++ image/views.py
@@ -1,7 +1,9 @@
-import PIL
-from django.shortcuts import render,redirect
+from PIL import Image as PIL_Image
+from PIL import ImageColor
+from django.shortcuts import render, redirect
 from .form import ImageForm
 from .models import Image
+
 
 def index(request):
     if request.method == "POST":
@@ -9,54 +11,52 @@ def index(request):
         if form.is_valid():
             form.save()
             obj = form.instance
-            return render(request, "index.html", {"obj": obj})
+            return render(request, "index.html", {"obj": obj, "current_img_id": obj.pk})
+    elif request.method == "GET":
+        current_img_id = request.GET.get('current_img_id')
+        wanted_color = request.GET.get('wanted_color')
+        if current_img_id is None:
+            form = ImageForm()
+            img = Image.objects.all()
+            return render(request, "index.html", {"img": img, "form": form})
+        elif wanted_color and current_img_id:
+            current_img = Image.objects.get(pk=current_img_id)
+            black_white = pixels(current_img)
+            res_color = count_color(current_img, wanted_color)
+            return render(request, "index.html", {"obj": current_img, "bw": black_white, "my_color_pixels": res_color, "current_img_id": current_img_id})
+        else:
+            current_img = Image.objects.get(pk=current_img_id)
+            black_white = pixels(current_img)
+            return render(request, "index.html", {"obj": current_img, "bw": black_white, "current_img_id": current_img_id})
     else:
         form = ImageForm()
         img = Image.objects.all()
-    return render(request,"index.html", {"img": img, "form": form})
+        return render(request, "index.html", {"img": img, "form": form})
 
-def pixels(request):
-    if request.GET.get('action'):
-        image = Image.open('img-thumbnail')  # Открываем изображение
-        width = image.size[0]  # Определяем ширину
-        height = image.size[1]  # Определяем высоту
-        pix = image.load()  # Выгружаем значения пикселей
-        black = 0
-        white = 0
-        p = 0
-        for x in range(width):
-            for y in range(height):
-                p = p + 1
-                if y == (0, 0, 0):
-                    black += 1
-                elif y == (255, 255, 255):
-                    white += 1
-        result=0
-        if black>white:
-            result="more black"
-        else:
-            result="more white"
 
-    return render(request,"index.html", {'bw':result})
-def pixels(request):
-    image = Image.open('img-thumbnail')  # Открываем изображение
-    width = image.size[0]  # Определяем ширину
-    height = image.size[1]  # Определяем высоту
-    pix = image.load()  # Выгружаем значения пикселей
+def pixels(img):
+    image = PIL_Image.open(img.image)
     black = 0
     white = 0
-    p = 0
-    for x in range(width):
-        for y in range(height):
-            p = p + 1
-            if y == (0, 0, 0):
-                black += 1
-            elif y == (255, 255, 255):
-                white += 1
-    result=0
-    if black>white:
-        result="more black"
+    for pixel in image.getdata():
+        if pixel == (0, 0, 0):
+            black += 1
+        elif pixel == (255, 255, 255):
+            white += 1
+    if black > white:
+        result = "more black"
+    elif black < white:
+        result = "more white"
     else:
-        result="more white"
+        result = "equal black and white"
+    return result
 
-    return render(request,"index.html", {'bw':"result"})
+
+def count_color(img, color_hex):
+    image = PIL_Image.open(img.image)
+    color_pix = 0
+    my_color = ImageColor.getcolor('#' + str(color_hex), "RGB")
+    for pixel in image.getdata():
+        if pixel == my_color:
+            color_pix += 1
+    return color_pix
